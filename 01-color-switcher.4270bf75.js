8f5bfa7785952c606f7896c4a084d73f835e36d5
MarioKarv <113836248+MarioKarv@users.noreply.github.com>
2023-03-15 14:49:17 +0000

    Deploying to gh-pages from @ MarioKarv/goit-js-hw-09@ec878c317f140f3d82408efac129040378775c7a 🚀

---
+++ 01-color-switcher.4270bf75.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");intervalId=null,t.addEventListener("click",(function(){intervalId=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(intervalId),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.4270bf75.js.map
